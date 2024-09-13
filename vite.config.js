8504c770530b5823e86cda047d285371b6bf8e39
Facundo Marchionna <tmfacundo@gmail.com>
2024-09-12 23:58:04 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://facundotm.github.io/Portfolio/",
+  plugins: [react()]
 });
